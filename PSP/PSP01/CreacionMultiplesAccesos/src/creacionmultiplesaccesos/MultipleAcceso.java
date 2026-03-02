a73fa49158cbee797b03544791b01fdb274e9de1
1yk0s <1yk0s.amar0kk@gmail.com>
2026-03-02 12:54:44 +0100

    Finalizado el proyecto CreacionMultiplesAccesos

--- PSP/PSP01/CreacionMultiplesAccesos/src/creacionmultiplesaccesos/MultipleAcceso.java
+++ PSP/PSP01/CreacionMultiplesAccesos/src/creacionmultiplesaccesos/MultipleAcceso.java
@@ -5,10 +5,45 @@
  */
 package creacionmultiplesaccesos;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
 /**
  *
  * @author 1yk0s
  */
 public class MultipleAcceso {
-    
+    public static void main(String[] args) {
+        Process nuevoProceso; //Se define una variable de tipo Process
+        
+        try {
+            PrintStream ps = new PrintStream(
+                new BufferedOutputStream(
+                    new FileOutputStream(
+                        new File("javalog.txt"), true)), true);
+            System.setOut(ps); //Sustituyo todas las salidas estándar
+            System.setErr(ps); //Sustituyo todas las salidas de error estándar
+            
+            for(int i = 0; i <= 20; i++) {
+                nuevoProceso = Runtime.getRuntime().exec("java -jar "+
+                        "CreacionMultiplesAccesos.jar " + i + " nuevo.txt");
+                /*
+                    Creo el nuevo proceso y le indico el número de orden y
+                    el fichero que debe utilizar.
+                */
+                
+                System.out.println("Creado el proceso " + i);
+                //uestro en consola que he creado otro proceso.
+            }
+            
+        } catch (SecurityException ex) {
+            System.err.println("Ha ocurrido un error de seguridad" +
+                    "No se ha podido crear el proceso por falta de permisos");
+        } catch (Exception ex) {
+            System.err.println("Ha ocurrido un error, descripción: " +
+                    ex.toString());
+        }
+    }
 }
