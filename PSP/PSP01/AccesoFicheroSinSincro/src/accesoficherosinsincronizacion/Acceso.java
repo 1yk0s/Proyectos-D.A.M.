7a4f5981b37877d5684e48b1eeb40c6394a4b15c
1yk0s <1yk0s.amar0kk@gmail.com>
2026-03-02 11:39:02 +0100

    Finalizado proyecto AccesoFicheroSinSincro

--- PSP/PSP01/AccesoFicheroSinSincro/src/accesoficherosinsincronizacion/Acceso.java
+++ PSP/PSP01/AccesoFicheroSinSincro/src/accesoficherosinsincronizacion/Acceso.java
@@ -6,8 +6,10 @@
 package accesoficherosinsincronizacion;
 
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.PrintStream;
 import java.io.PrintWriter;
@@ -24,8 +26,11 @@ public class Acceso {
         String osName; //Nombre del sistema operativo.
         String nombreFichero = null;    //Nombre del fichero que se utiliza
         File archivo = null; //Acceso al fichero
-        FileWriter escribir = null; //
-        PrintWriter pw = null; //
+        FileWriter escribir = null; //Abre la puerta al archivo para escribir 
+        PrintWriter pw = null; //envuelve FileWriter para proporcionar métodos como println().
+        FileReader leer = null; //Abre flujo de caracteres hacia el archivo
+        BufferedReader br = null; //Acumula datos en una parte de la RAM(buffer)
+        int valor = 0; //
         
        //Se comrpueba si estoy recibiendo argumentos en la línea de comandos
        if(args.length > 0) {
@@ -90,13 +95,54 @@ public class Acceso {
        }
        //Se lee el fichero
        try {
-           
+           leer = new FileReader(archivo);
+           br = new BufferedReader(leer);
+           //Lectura del fichero.
+           String linea;
+           linea = br.readLine();
+           valor = Integer.parseInt(linea);
+           System.out.println("Proceso"+ orden + 
+                   ": Valor leído del fichero: " + valor);
        } catch (Exception e) {
            System.err.println("P"+orden+" Error al leer del fichero");
+       } finally {
+           /*
+            En el finally cierro el escribir, para asegurarme
+            que se cierra tanto si todo va bien como si salta
+            alguna excepción.
+           */
+           try {
+               if (leer != null) {
+                   leer.close();
+               }
+           } catch (Exception e2) {
+               System.err.println("P"+orden+" Error al cerrar el fichero");
+               System.exit(1); //Si hay error, finaliza.
+           }
        }
        
+       //Incremento
+       valor++;
        
-        
+       //Escritura en fichero
+       try {
+           escribir = new FileWriter(nombreFichero);
+           pw = new PrintWriter(escribir);
+           pw.println(String.valueOf(valor));
+           System.out.println("Proceso"+ orden +
+                   ": Valor escrito en el fichero: " + valor);
+       } catch (Exception e) {
+           System.err.println("P"+orden+" Error al escribir en el fichero");
+       } finally {
+           try {
+               //Me aseguro de que se cierra el fichero
+               if( escribir != null) {
+                   escribir.close();
+               }
+           } catch (Exception e2) {
+               System.err.println("P"+orden+" Error al cerrar el fichero");
+               System.exit(1); //Si hay error, finalizo.
+           }
+       }
     }
-    
 }
