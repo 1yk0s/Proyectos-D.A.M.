699ddbd9428bbf4e41cd8c7659672f57f4f9ea61
Lykos <1yk0s.amar0kk@gmail.com>
2026-04-01 14:04:44 +0200

    Finalización de ejercicios de ejemplo de pdf UT1

--- PSP/PSP01/CreacionDeUnProcesoTres/src/creaciondeunprocesotres/CreacionDeUnProcesoTres.java
+++ PSP/PSP01/CreacionDeUnProcesoTres/src/creaciondeunprocesotres/CreacionDeUnProcesoTres.java
@@ -5,6 +5,11 @@
  */
 package creaciondeunprocesotres;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author kevin
@@ -16,6 +21,50 @@ public class CreacionDeUnProcesoTres {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Runtime r = Runtime.getRuntime();
+        String comando = "CMD /C DIRR";
+        Process p = null;
+        
+        try {
+            p = r.exec(comando);
+            
+            InputStream is = p.getInputStream();
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader (is));
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
+            }
+            
+            br.close();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        try {
+            
+            InputStream er = p.getErrorStream();
+            BufferedReader brer = new BufferedReader(
+                    new InputStreamReader (er));
+            String liner;
+            while ((liner = brer.readLine()) != null) {
+                System.out.println("ERROR > " + liner);
+            }
+            brer.close();
+            
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        
+        //COMPROBACIÓN DE ERROR 0 BIEN, 1 MAL
+        int exitVal;
+        
+        try {
+            exitVal = p.waitFor();
+            System.out.println("Valor de Salida: " + exitVal);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
-    
 }
